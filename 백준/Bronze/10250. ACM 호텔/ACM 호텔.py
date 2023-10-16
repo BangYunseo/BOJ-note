37bf9f35d3f747c898ea15fcad0037992c533516
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2023-10-16 21:31:18 +0900

    Update ACM 호텔.py

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,12 +1,14 @@
 T = int(input())
+
 for i in range(T):
-    
     H, W, N = map(int,input().split())
+    
     Room_Floor = N % H * 100
     Room_Num = N // H + 1
+    
     if N % H == 0:
         Room_Floor = H * 100
         Room_Num -= 1
+        
     Key = Room_Floor + Room_Num
-
     print(Key)
