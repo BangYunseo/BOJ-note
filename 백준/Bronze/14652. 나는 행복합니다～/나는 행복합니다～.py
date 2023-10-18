58542439c86e03883d1c8febf19e0990f83951d9
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2023-10-18 20:28:11 +0900

    Update 나는 행복합니다～.py

--- 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.py
+++ 백준/Bronze/14652. 나는 행복합니다～/나는 행복합니다～.py
@@ -1,4 +1,5 @@
 N, M, K= map(int,input().split())
-a = K // M
-b = K % M
-print(a, b)
+# 3개의 수를 입력받음
+a = K // M      # 관중석 번호를 M으로 나눈 몫이 열임
+b = K % M       # 관중석 번호를 M으로 나눈 나머지가 행
+print(a, b)    # 출력
