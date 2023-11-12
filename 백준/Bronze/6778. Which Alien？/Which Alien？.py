ea7869dbf99a0da7ef066203481cdd6c2fe8e739
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2023-11-12 20:47:19 +0900

    [Bronze IV] Title: Which Alien?, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+if a >= 3:
+    if b <= 4:
+        print("TroyMartian")
+if a <= 6:
+    if b >= 2:
+        print("VladSaturnian")
+if a <= 2:
+    if b <= 3:
+        print("GraemeMercurian")
