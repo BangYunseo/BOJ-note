fdaa17e994730cfb2317f214d464e95be7db452f
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-01-08 19:23:48 +0900

    [Bronze I] Title: 0의 개수, Time: 4012 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/11170. 0의 개수/0의 개수.py
+++ 백준/Bronze/11170. 0의 개수/0의 개수.py
@@ -1,6 +1,8 @@
+import sys
+
 for i in range(int(input())):
     zerocount = 0
-    N, M = map(int, input().split())
+    N, M = map(int, sys.stdin.readline().split())
     for x in range(N, M + 1):
         for y in str(x):
             if y == '0':
