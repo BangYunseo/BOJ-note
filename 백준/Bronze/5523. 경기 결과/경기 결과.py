dd4d6c2db5da4ae335902b6597971d1195de5e54
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-01-20 23:29:49 +0900

    [Bronze III] Title: 경기 결과, Time: 864 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/5523. 경기 결과/경기 결과.py
+++ 백준/Bronze/5523. 경기 결과/경기 결과.py
@@ -1,7 +1,7 @@
+import sys
 countA = countB = 0
-
-for i in range(int(input())):
-    A, B = map(int, input().split())
+for i in range(int(sys.stdin.readline())):
+    A, B = map(int, sys.stdin.readline().split())
     if A > B:
         countA += 1
     elif A < B :
