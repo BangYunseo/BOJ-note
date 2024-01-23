72b957f7c3065e0c8525df436267f75cb71008e2
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-01-23 22:05:04 +0900

    [Silver V] Title: 너의 평점은, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.py
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -1,26 +1,12 @@
 summ = scores = 0
 
+ratinglist = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+gradelist = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0.0]
+
 for i in range(20):
     name, score, grade = input().split()
-    scores += float(score)
-    if grade == 'A+':
-        summ += float(score) * 4.5
-    elif grade == 'A0':
-        summ += float(score) * 4.0
-    elif grade == 'B+':
-        summ += float(score) * 3.5
-    elif grade == 'B0':
-        summ += float(score) * 3.0
-    elif grade == 'C+':
-        summ += float(score) * 2.5
-    elif grade == 'C0':
-        summ += float(score) * 2.0
-    elif grade == 'D+':
-        summ += float(score) * 1.5
-    elif grade == 'D0':
-        summ += float(score) * 1.0
-    elif grade == 'F':
-        summ += float(score) * 0.0
-    else:
-        scores -= float(score)
-print(summ / scores)
+    score = float(score)
+    if grade != 'P':
+        scores += score
+        summ += score * gradelist[ratinglist.index(grade)]
+print('%.6f' %(summ / scores))
