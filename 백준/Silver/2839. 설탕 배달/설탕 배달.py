23fcb360f71bbda557763f14e32d54e90c15710e
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-02-04 21:28:57 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,13 +1,12 @@
 N = int(input())
 
-count = 0
+bag = 0
 while N >= 0:
     if N % 5 == 0:
-        count += int(N//5)
-        print(count)
+        bag += int(N//5)
+        print(bag)
         break
     N -= 3
-    count += 1
-
+    bag += 1
 else:
     print(-1)
