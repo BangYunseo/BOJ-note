0ba3f7eb62c8d783ea0625177c3466b06bdbfdd5
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-03-02 17:22:51 +0900

    [Bronze III] Title: 쌍의 합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5217. 쌍의 합/쌍의 합.py
+++ 백준/Bronze/5217. 쌍의 합/쌍의 합.py
@@ -1,9 +1,7 @@
-T = int(input())
-for i in range(T):
+for i in range(int(input())):
     a = int(input())
     f = 1
-    print("Pairs for %d:" %a , end = ' ')
-    
+    print("Pairs for %d:" %(a) , end = ' ') 
     for j in range((a - 1) // 2):
         if j != 0:
             print(',', end = ' ')
