122a29e7c690915171f8e41930b95dc78dadc8ca
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-03-02 16:50:51 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,16 +1,24 @@
+import sys
+input = sys.stdin.readline
+
 N, M = map(int, input().split())
+
 arrA = []
 arrB = []
 res = []
+
 for i in range(N):
     arr1 = list(map(int, input().split()))
     arrA.append(arr1)
+    
 for j in range(N):
     arr2 = list(map(int, input().split()))
     arrB.append(arr2)
+    
 for a in range(N):
     for b in range(M):
         res.append(arrA[a][b] + arrB[a][b])
+        
 for x in range(0, len(res), M):
     for y in range(0, M):
         print(res[x + y], end = ' ')
