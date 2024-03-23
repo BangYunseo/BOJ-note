82616d1c9b1c072d9b362e56d62408ac88ca9de6
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-03-23 17:19:41 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,23 +1,22 @@
 #include <iostream>
-#include<cmath>
-
+#include <string>
+#include <cmath>
+#define r 31
+#define M 1234567891
 using namespace std;
 
-char arr[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 int main(){
-    int r = 31;
-    long long M = 1234567891;
-    int L;
-    int res = 0;
+    long long res = 0;
+    long long a = 1;
     string word;
+    int L;
     cin >> L;
     cin >> word;
-    
+
     for(int i = 0; i < L; i++){
-        for(int j = 0; j < 26; j++){
-            if (word[i] == arr[j])
-                res += (j + 1) * pow(r, i);
-        }
+        res += ((long(word[i]) - 96) * a) % M;
+        a = (a * r) % M; 
     }
+    res %= M;
     cout << res;
 }
