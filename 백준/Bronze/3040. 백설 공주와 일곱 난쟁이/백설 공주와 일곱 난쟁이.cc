ee054aac73ad4df3a1cdec3e39c155b58e9f824a
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-04-24 21:17:29 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.cc
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.cc
@@ -4,22 +4,24 @@ using namespace std;
 
 int main(){
     int arr[9], sum = 0;
+    int a, b;
+    
     for(int i = 0; i < 9; i++){
         cin >> arr[i];
         sum += arr[i];
     }
-    sum -= 100;
-    int i, j;
-    for(i = 0; i < 9; i++){
-        for(j = i + 1; j < 9; j++){
-            if(arr[i] + arr[j] == sum){
-                arr[i] = arr[j] = 0;
+    
+    for(int i = 0; i < 9; i++){
+        for(int j = i + 1; j < 9; j++){
+            if(sum - (arr[i] + arr[j]) == 100){
+                a = i; 
+                b = j;
                 break;
             }
         }
-        if((arr[i] == 0) && (arr[j] == 0)) break;
     }
-    for(int i = 0; i < 9; i++){
-        if(arr[i] != 0)  cout << arr[i] << endl;
+    for(int k = 0; k < 9; k++){
+        if(k == a || k == b) continue;
+        else cout << arr[k] << endl;
     }
 }
