cbe3e62a5379d4a721dfca2ff803084dcb25873b
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-04-26 18:46:52 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -4,7 +4,6 @@ using namespace std;
 int a1, a0, c, n0;
 int main(){
     cin >> a1 >> a0 >> c >> n0;
-    
-    if(((a1 * n0) + a0 <= c * n0) && (a1 <= c)) cout << 1 << endl;
-    else cout << 0 << endl;
+    if(((a1 * n0) + a0 <= c * n0) && (a1 <= c)) cout << 1 << '\n';
+    else cout << 0 << '\n';
 }
