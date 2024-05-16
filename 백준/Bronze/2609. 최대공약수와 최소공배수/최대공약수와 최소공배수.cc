ea9ba82493b27e108f48e57e619a569c74fed563
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-05-16 23:20:58 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.cc
@@ -3,18 +3,19 @@
 using namespace std;
 
 int GCD(int a, int b){
-    if(b != 0){
-        int temp1 = b;
-        int temp2 = a % b;
-        return GCD(temp1, temp2);
-    }else{
-        return a;
+    int r = a % b;
+    while(r != 0){
+        a = b;
+        b = r;
+        r = a % b;
     }
+    return b;
 }
 
 int main(){
     int A, B;
     cin >> A >> B;
-    int res = GCD(A, B);
-    cout << res << endl << ((A * B) / res);
+    
+    int temp = GCD(A, B);
+    cout << temp << endl << A * B / temp; 
 }
