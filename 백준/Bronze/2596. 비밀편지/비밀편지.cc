a909e6c89ec546c7a913ed8a788509ad5cbcc4e8
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-05-16 23:13:00 +0900

    [Bronze I] Title: 비밀편지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/2596. 비밀편지/비밀편지.cc
+++ 백준/Bronze/2596. 비밀편지/비밀편지.cc
@@ -1,41 +1,38 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
+
 using namespace std;
 
-int N;
-string arr[8] = {"000000", "001111", "010011", "011100", 
-                 "100110", "101001", "110101", "111010"};
+string arr[8] = {"000000", "001111", "010011", "011100", "100110", "101001", "110101", "111010"};
 int main(){
-    cin >> N;
+    int T;
+    cin >> T;
     
-    string word, res("");
-    cin >> word;
-    
-    for(int i = 0; i < N; i++){
+    string let, res("");
+    cin >> let;
+    for(int i = 0; i < T; i++){
         int check = 0;
-        
         for(int j = 0; j < 8; j++){
             int count = 0;
             for(int k = 0; k < 6; k++){
-                if (word[k] != arr[j][k]){
+                if(let[k] != arr[j][k]){
                     count++;
-                    if(count > 1) break;
+                    if(count > 1)
+                        break;
                 }
             }
-            
-            if(count == 0 || count == 1){
+            if((count == 0) || (count == 1)){
                 res += j + 65;
                 check = 1;
                 break;
             }
         }
-        
-        if(check == 0) {
-            cout << i + 1;
+        if(check == 0){
+            cout << i + 1 << '\n';
             return 0;
         }
-        else word.erase(0, 6);
+        else{
+            let.erase(0, 6);
+        }
     }
-    cout << res;
+    cout << res << "\n";
 }
