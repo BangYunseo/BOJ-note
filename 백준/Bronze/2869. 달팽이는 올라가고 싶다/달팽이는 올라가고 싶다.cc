0b32fa1fa5761d11b0e54dfd4f09787420475ed5
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-05-27 20:48:11 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -5,9 +5,8 @@ using namespace std;
 int A, B, V;
 int main(){
     cin >> A >> B >> V;
-    int res = 1;
-    res += (V - A) / (A - B);
-    if((V - A) % (A - B) != 0) res++;
-    if (A >= V) cout << '1';
-    else cout << res;
+    int res = (V - A) / (A - B);
+    if((V - A) % (A - B) == 0) res++;
+    else res += 2;
+    cout << res;
 }
