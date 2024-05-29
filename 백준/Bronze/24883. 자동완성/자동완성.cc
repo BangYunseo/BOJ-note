5b8d1884f2a31b1b386e8d2af35373645e11ec09
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-05-29 18:48:12 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    string word;
+    cin >> word;
+    if(word == "n" || word == "N"){
+        cout << "Naver D2";
+    }
+    else cout << "Naver Whale";
+}
