8ab54d77c00370d40b8c1cb5242dd402904d97a3
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-05-31 20:06:55 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int T, k, n;
+    
+    int arr[101][15];
+    
+    cin >> T;
+    while(T--){
+        cin >> k >> n;
+        for(int i = 0; i <= k; i++){
+            for(int j = 1; j <= n; j++){
+                if(i == 0) arr[0][j] = j;
+                else if(j == 1) arr[i][1] = 1;
+                else arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
+            }
+        }
+        cout << arr[k][n] << endl;
+    }
+}
