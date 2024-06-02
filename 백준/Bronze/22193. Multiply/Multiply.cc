c3da30794a80fe0476913f1b814266b7c6966cb0
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-06-02 15:12:35 +0900

    [Bronze V] Title: Multiply, Time: 0 ms, Memory: 2020 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long int res1, res2, res;
+    int N, M;
+    cin >> N >> M >> res1 >> res2;
+    res = res1 * res2;
+    cout << res;
+}
