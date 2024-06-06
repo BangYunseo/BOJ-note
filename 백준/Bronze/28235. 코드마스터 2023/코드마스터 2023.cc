4b683cc9d96f254187b618c8d55498adee938555
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-06-06 11:45:57 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+string word;
+int main(){
+    cin >> word;
+    if(word == "SONGDO"){
+        cout << "HIGHSCHOOL";
+    }
+    else if(word == "CODE"){
+        cout << "MASTER";
+    }
+    else if(word == "2023"){
+        cout << "0611";
+    }
+    else if(word == "ALGORITHM"){
+        cout << "CONTEST";
+    }
+}
