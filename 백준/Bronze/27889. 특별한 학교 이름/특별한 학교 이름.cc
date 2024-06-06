ad74301b56a26a95ee15f7b4032e9088aa89e05c
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-06-06 11:44:52 +0900

    [Bronze V] Title: 특별한 학교 이름, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27889. 특별한 학교 이름/특별한 학교 이름.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+string word;
+int main(){
+    cin >> word;
+    if(word == "NLCS"){
+        cout << "North London Collegiate School";
+    }
+    else if(word == "BHA"){
+        cout << "Branksome Hall Asia";
+    }
+    else if(word == "KIS"){
+        cout << "Korea International School";
+    }
+    else if(word == "SJA"){
+        cout << "St. Johnsbury Academy";
+    }
+}
