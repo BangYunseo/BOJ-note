80013dd093c59e65be6c6fa3b2a80e7d35efa5d7
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-06-08 19:58:45 +0900

    [Bronze V] Title: Copier, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26574. Copier/Copier.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int T, temp;
+    cin >> T;
+    
+    for(int i = 0; i < T; i++){
+        cin >> temp;
+        cout << temp << ' ' << temp << endl;
+    }
+}
