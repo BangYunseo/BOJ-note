5f081436b6e040cf2c428f2f5b15ffb57d63380a
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-06-13 08:55:05 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N, A, B;
+    cin >> N >> A >> B;
+    
+    if(A < B) cout << "Bus";
+    else if(A == B) cout << "Anything";
+    else cout << "Subway";
+}
