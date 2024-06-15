92c0b021f6ce02e0b6aa6ee6f0fedd559735c2b4
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-06-15 13:03:35 +0900

    [Bronze V] Title: Adding Trouble, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/31654. Adding Trouble/Adding Trouble.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    long long int A, B, C;
+    cin >> A >> B >> C;
+    if(A + B == C)
+        cout << "correct!";
+    else
+        cout << "wrong!";
+}
