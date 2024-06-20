f7e6abe0c546109e954d863bebe6a19a43dfca70
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-06-20 20:13:52 +0900

    [Silver IV] Title: 배열 전체 탐색하기, Time: 128 ms, Memory: 2800 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/26168. 배열 전체 탐색하기/배열 전체 탐색하기.cc
+++ 백준/Silver/26168. 배열 전체 탐색하기/배열 전체 탐색하기.cc
@@ -1,32 +1,23 @@
 #include <iostream>
-
+#include <algorithm> // sort와 lower_bound, upper_bound를 사용하기 위함
 using namespace std;
 
-long long arr[100004];
+long long int arr[100004];
 int n, m;
 
-int A(long long num){
-    int count = 0;
-    for(int i = 0; i < n; i++){
-        if(arr[i] >= num) count++;
-    }
-    return count;
+int A(long long int num){
+    // lower_bound를 사용하여 num보다 크거나 같은 첫 번째 위치를 찾음
+    return n - (lower_bound(arr, arr + n, num) - arr);
 }
 
-int B(long long num){
-    int count = 0;
-    for(int i = 0; i < n; i++){
-        if(arr[i] > num) count++;
-    }
-    return count;
+int B(long long int num){
+    // upper_bound를 사용하여 num보다 큰 첫 번째 위치를 찾음
+    return n - (upper_bound(arr, arr + n, num) - arr);
 }
 
-int C(long long num1, long long num2){
-    int count = 0;
-    for(int i = 0; i < n; i++){
-        if((arr[i] >= num1) && (arr[i] <= num2)) count++;
-    }
-    return count;
+int C(long long int num1, long long int num2){
+    // lower_bound와 upper_bound를 사용하여 범위 내의 원소 개수를 찾음
+    return upper_bound(arr, arr + n, num2) - lower_bound(arr, arr + n, num1);
 }
 
 int main(){
@@ -35,27 +26,29 @@ int main(){
     cout.tie(NULL);
 
     cin >> n >> m;
-    int temp, temp2;
-    for(long long i = 0; i < n; i++){
+    for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
+
+    // 배열을 정렬
+    sort(arr, arr + n);
     
     for(int j = 0; j < m; j++){
         int sel;
         cin >> sel;
         
         if(sel == 1){
-            long long temp;
+            long long int temp;
             cin >> temp;
             cout << A(temp) << '\n';
         }
         else if(sel == 2){
-            long long temp;
+            long long int temp;
             cin >> temp;
             cout << B(temp) << '\n';
         }
         else if(sel == 3){
-            long long temp1, temp2;
+            long long int temp1, temp2;
             cin >> temp1 >> temp2;
             cout << C(temp1, temp2) << '\n';
         }
