a708a18c96e009981f9acb015ede19d05640b2e6
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-07-31 13:40:54 +0900

    [Bronze II] Title: 분해합, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.cc
+++ 백준/Bronze/2231. 분해합/분해합.cc
@@ -1,28 +1,26 @@
 #include <iostream>
-#include <algorithm>
 
 using namespace std;
 
-int N;
-
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    cin >> N;
+    int N;
+    cin >> N; 
+    
     int res = 0;
     for(int i = 1; i < N; i++){
         int sum = 0;
         int num = i;
-        while(num != 0){
+        while(true){
+            if(num == 0)
+                break;
             sum += num % 10;
             num /= 10;
         }
+        
         if(sum + i == N){
             res = i;
             break;
         }
     }
-
     cout << res;
 }
