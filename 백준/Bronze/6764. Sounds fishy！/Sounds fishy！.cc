e19a0e912b2a828446d599a1990aa4395982d4fa
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-08-11 13:11:05 +0900

    [Bronze IV] Title: Sounds fishy!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.cc
+++ 백준/Bronze/6764. Sounds fishy！/Sounds fishy！.cc
@@ -2,30 +2,15 @@
 
 using namespace std;
 
+int dept_arr[4];
 int main(){
-    int dept_arr[4];
-    bool asc = false, desc = false, same = false;
-    cin >> dept_arr[0] >> dept_arr[1];
+    for(int i = 0; i < 4; i++) cin >> dept_arr[i];
     
-    if(dept_arr[1] > dept_arr[0]) asc = true;
-    else if(dept_arr[1] == dept_arr[0]) same = true;
-    else if(dept_arr[1] < dept_arr[0]) desc = true;
-    
-    for(int i = 2; i <= 3; i++){
-        cin >> dept_arr[i];
-        if(asc){
-            if(dept_arr[i] <= dept_arr[i - 1]) asc = false;
-        }
-        else if(same){
-            if(dept_arr[i] != dept_arr[i - 1]) same = false;
-        }
-        else if(desc){
-            if(dept_arr[i] >= dept_arr[i - 1]) desc = false;
-        }
-    }
-
-    if(asc) cout << "Fish Rising";
-    else if(desc) cout << "Fish Diving";
-    else if(same) cout << "Fish At Constant Depth";
+    if(dept_arr[0] > dept_arr[1] && dept_arr[1] > dept_arr[2] &&
+      dept_arr[2] > dept_arr[3]) cout << "Fish Diving";
+    else if(dept_arr[0] < dept_arr[1] && dept_arr[1] < dept_arr[2] &&
+           dept_arr[2] < dept_arr[3]) cout << "Fish Rising";
+    else if(dept_arr[0] == dept_arr[1] && dept_arr[1] == dept_arr[2] &&
+           dept_arr[2] == dept_arr[3]) cout << "Fish At Constant Depth";
     else cout << "No Fish";
 }
