ddcf9507e7c94e88d289014e2fd5fb060d208b0d
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-08-11 16:10:32 +0900

    [Bronze IV] Title: Terms of Office, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6888. Terms of Office/Terms of Office.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int X, Y;
+    cin >> X >> Y;
+
+    for(int i = X; i <= Y; i+= 60){
+        cout << "All positions change in year " << i << endl;
+    }
+}
