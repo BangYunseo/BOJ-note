8bd9d2748baa43e0050d72d594ece1ef84b56a19
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-08-12 21:44:58 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int month, day;
+    cin >> month >> day;
+    
+    if(month < 2 || (month == 2 && day < 18)) cout << "Before";
+    else if(month == 2 && day == 18) cout << "Special";
+    else cout << "After";
+}
