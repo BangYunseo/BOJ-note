ef2f7216a0f70063431ff3c5b61e46e30a5751ea
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-08-15 15:59:02 +0900

    [Bronze IV] Title: Quadrilateral, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10188. Quadrilateral/Quadrilateral.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n, a, b;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++){
+        cin >> a >> b;
+        for(int j = 0; j < b; j++){
+            for(int k = 0; k < a; k++){
+                cout << "X";
+            } cout << endl;
+        }
+        cout << endl;
+    }
+}
