86d84b9d0645853db9080ba0bd0ee06e2b0394ac
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-08-25 19:57:20 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int T; cin >> T;
+    
+    long long int A, B;
+    while(T--){
+        cin >> A >> B;
+        cout << "yes\n";
+    }
+}
