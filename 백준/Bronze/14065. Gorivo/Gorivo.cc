f4ae351fb00b35ec5ae8271199e9658ff61c578d
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-08-30 21:42:02 +0900

    [Bronze IV] Title: Gorivo, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/14065. Gorivo/Gorivo.cc
+++ 백준/Bronze/14065. Gorivo/Gorivo.cc
@@ -2,18 +2,13 @@
 
 using namespace std;
 
-const double GALLON2LITER = 3.785411784;
-const double MILE2KM = 1.609344;
-const double HUNDRED_KM = 100.00;
-        
-int main(void){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     
-    double milesPerGallon;
-    cin >> milesPerGallon;
+    double car_mile;
+    cin >> car_mile;
     
-    double result = HUNDRED_KM / ((MILE2KM / GALLON2LITER) * milesPerGallon);
-    printf("%.6lf\n", result);
-    return 0;
+    double res = 100.00 / ((1.609344 / 3.785411784) * car_mile);
+    printf("%.6lf\n", res);
 }
