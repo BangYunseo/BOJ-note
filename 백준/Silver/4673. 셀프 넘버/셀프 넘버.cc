d243e716f0f1597bb8ab2cb735e600507c8eb293
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-09-18 15:12:47 +0900

    [Silver V] Title: 셀프 넘버, Time: 24 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.cc
@@ -1,28 +1,25 @@
 #include <iostream>
-#define MAX 10001
+#include <string>
+
 using namespace std;
 
-int func(int num){
-    int number = num;
-    
-    while(num != 0){
-        number = number + (num % 10);
-        num = num / 10;
-    }
-    
-    return number;
-}
 int main(){
-    int n = 0;
-    bool check[MAX] = {false, };
-    
-    for(int i = 1; i < MAX; i++){
-        n = func(i);
-        
-        if(n < 10001) check[n] = true;
+    int arr[10001] = {0, };
+    string word;
+    for(int i = 1; i < 10001; i++){
+        int temp = 0;
+        word = to_string(i);
+        if(word.length() == 1) 
+            temp = i * 2;
+        else if(word.length() == 2) 
+            temp = (word[0] - '0') + (word[1] - '0') + i;
+        else if(word.length() == 3) 
+            temp = (word[0] - '0') + (word[1] - '0') + (word[2] - '0') + i;
+        else if(word.length() == 4)
+            temp = (word[0] - '0') + (word[1] - '0') + (word[2] - '0') + (word[3] - '0') + i;
+        if(temp < 10001) arr[temp]++;
     }
-    
-    for(int i = 1; i < MAX; i++){
-        if(check[i] == false) cout << i << endl;
+    for(int i = 1; i < 10001; i++){
+        if(arr[i] == 0) cout << i << endl;
     }
 }
