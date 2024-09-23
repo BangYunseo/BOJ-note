907c6d7e2c5435ff4c61cbed9f872571dacc3eeb
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-09-23 21:58:02 +0900

    [Silver V] Title: K번째 수, Time: 992 ms, Memory: 21552 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int N, K;
+    cin >> N >> K;
+    
+    vector<int> arr(N);
+    for(int i = 0; i < N; i++) cin >> arr[i];
+    sort(arr.begin(), arr.end());
+    cout << arr[K - 1];
+}
