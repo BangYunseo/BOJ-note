8e108d25a7c41e752d4bfaa7cee3fa21ec860a47
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-09-24 23:16:56 +0900

    [Bronze I] Title: 옷걸이, Time: 64 ms, Memory: 6908 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/27951. 옷걸이/옷걸이.cc
+++ 백준/Bronze/27951. 옷걸이/옷걸이.cc
@@ -3,50 +3,61 @@
 
 using namespace std;
 
-int main(){
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    cout.tie(NULL);
-    
+
     int N, U, D;
     cin >> N;
 
-    string word = "";
     vector<int> arr(N);
-    
-    for(int i = 0; i < N; i++) cin >> arr[i];
-    cin >> U >> D;
-    
-    for(int i = 0; i < N; i++){
-        if(arr[i] == 1){
-            if(U > 0){
-                U--; 
-                word += 'U';
-            }
-            else{
-                cout << "NO";
-                return 0;
-            }
-        } else if(arr[i] == 2){
-            if(D > 0){
-                D--;
-                word += 'D';
-            }
-            else{
-                cout << "NO";
-                return 0;
-            }
-        } else if(arr[i] == 3){
-            if(U > 0){
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i]; // 옷걸이 종류 입력
+    }
+    cin >> U >> D; // 상의와 하의 개수 입력
+
+    string word(N, ' '); // 결과 문자열 초기화
+
+    // 먼저 전용 옷걸이에 상의와 하의를 걸자
+    for (int i = 0; i < N; i++) {
+        if (arr[i] == 1) { // 상의 전용
+            if (U > 0) {
                 U--;
-                word += 'U';
+                word[i] = 'U';
+            } else {
+                cout << "NO\n";
+                return 0; // 상의 부족
             }
-            else if(D > 0){
+        } else if (arr[i] == 2) { // 하의 전용
+            if (D > 0) {
                 D--;
-                word += 'D';
+                word[i] = 'D';
+            } else {
+                cout << "NO\n";
+                return 0; // 하의 부족
             }
         }
     }
-    if(U == 0 && D == 0) cout << "YES\n" << word;
-    else cout << "NO";
+
+    // 이제 남은 상의와 하의 개수 확인
+    for (int i = 0; i < N; i++) {
+        if (arr[i] == 3) { // 둘 다 걸 수 있는 경우
+            if (U > 0) {
+                U--;
+                word[i] = 'U';
+            } else if (D > 0) {
+                D--;
+                word[i] = 'D';
+            }
+        }
+    }
+
+    // 모든 옷을 잘 걸었는지 확인
+    if (U == 0 && D == 0) {
+        cout << "YES\n" << word << "\n";
+    } else {
+        cout << "NO\n";
+    }
+
+    return 0;
 }
