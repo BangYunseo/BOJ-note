4dca906242b3fb87416728cbc18d4710d6a9c8e6
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-09-28 23:49:23 +0900

    [Bronze IV] Title: Congruent Numbers, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15128. Congruent Numbers/Congruent Numbers.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    long long q1, q2, p1, p2;
+    cin >> p1 >> q1 >> p2 >> q2;
+
+    long long top = p1 * p2;
+    long long bottom = q1 * q2 * 2;
+    if(top % bottom == 0) cout << 1;
+    else cout << 0;
+}
