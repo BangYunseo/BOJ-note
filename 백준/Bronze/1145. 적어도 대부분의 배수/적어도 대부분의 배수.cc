290a8a7cad3c3ad7b967c3ff6b6ed87609e51fa0
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-10-06 11:26:25 +0900

    [Bronze I] Title: 적어도 대부분의 배수, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.cc
+++ 백준/Bronze/1145. 적어도 대부분의 배수/적어도 대부분의 배수.cc
@@ -1,22 +1,21 @@
 #include <iostream>
-
+#include <algorithm>
 
 using namespace std;
 
 int main(){
-    int num[5];
-    int min = -1;
-    for(int i = 0; i < 5; i++){
-        cin >> num[i];
-        if(min == -1 || min > num[i]) min = num[i];
-    }
+    int arr[5];
+    for(int i = 0; i < 5; i++) cin >> arr[i]; 
 
-    int num_min = min;
-    while(true){
-        int count = 0;
-        for(int i = 0; i < 5; i++) if(num_min % num[i] == 0) count++;
+    sort(arr, arr + 5);
+    int count = 0;
+    int num = 1;
+    for(num = 1; num < 1000000; num++){
+        for(int j = 0; j < 5; j++){
+            if(num % arr[j] == 0) count++;
+        }
         if(count >= 3) break;
-        num_min++;
+        else count = 0;
     }
-    cout << num_min;
+    cout << num;
 }
