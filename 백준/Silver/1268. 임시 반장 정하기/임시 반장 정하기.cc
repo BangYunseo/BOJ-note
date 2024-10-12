4d4f1e3a8916015361ad8fa43f49cdb77b001a25
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-10-12 20:55:02 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 8 ms, Memory: 3024 KB -BaekjoonHub

---
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[1001][6] = {0, };
+bool check[1001][1001] = {false, };
+int main(){
+    int N;
+    cin >> N;
+
+    
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= 5; j++) 
+            cin >> arr[i][j];
+    }
+    
+    for(int i = 1; i <= 5; i++){
+        for(int j = 1; j <= N - 1; j++){
+            for(int k = j + 1; k <= N; k++){
+                if(k == j) continue;
+                
+                if(arr[j][i] == arr[k][i]){
+                    check[j][k] = true;
+                    check[k][j] = true;
+                }
+            }
+        }
+    }
+    
+    int max = 0;
+    int index = 1;
+    for(int i = 1; i <= N; i++){
+        int temp = 0;
+        for(int j = 1; j <= N; j++){
+            if(check[i][j] == true) temp++;
+        }
+        if(temp > max){
+            max = temp;
+            index = i;
+        }
+    }
+    cout << index;
+}
