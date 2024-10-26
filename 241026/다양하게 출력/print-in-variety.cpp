fdb0a67eb8dc72e4cebff0f7c5ca1ebe95eb8920
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-10-26 23:02:52 +0900

    Title: 다양하게 출력, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241026/다양하게 출력/print-in-variety.cpp
+++ 241026/다양하게 출력/print-in-variety.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
     string word;
-    cin >> word;
-    cout << word << endl;
+    while(getline(cin, word)) cout << word << endl;
     return 0;
 }
