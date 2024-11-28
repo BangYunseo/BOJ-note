2b139cf9030f4b36ac61f7aa2eafbd48883937e9
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2024-11-28 19:42:07 +0900

    [Gold V] Title: 토마토, Time: 92 ms, Memory: 7640 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.cc
+++ 백준/Gold/7569. 토마토/토마토.cc
@@ -1,90 +1,79 @@
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
-int N, M, H;
-int fruit[101][101][101]; // H M N
-int visited[101][101][101];
-int direci[6] = {0, 0, 0, 0, -1, 1};
-int direcj[6] = {0, -1, 0, 1, 0, 0};
-int direcy[6] = {1, 0, -1, 0, 0, 0};
+int M, N, H;
+
+int dx[6] = {1, -1, 0, 0, 0, 0};
+int dy[6] = {0, 0, -1, 1, 0, 0};
+int dz[6] = {0, 0, 0, 0, -1, 1};
+
+int arr[101][101][101] = {0};
 queue<pair<pair<int, int>, int>> q;
-void bfs()
-{
 
-    while (!q.empty())
-    {
-
-        int fronti = q.front().first.first;
-        int frontj = q.front().first.second;
-        int fronty = q.front().second;
+void bfs(){
+    
+    while(!q.empty()){
+        int nx = q.front().first.first;
+        int ny = q.front().first.second;
+        int nz = q.front().second;
+        
         q.pop();
-        for (int i = 0; i < 6; i++)
-        {
-            int pi = fronti + direci[i];
-            int pj = frontj + direcj[i];
-            int py = fronty + direcy[i];
-            if (pi >= 0 && pi < H && pj >= 0 && pj < M && py >= 0 && py < N)
-            {
-                if (fruit[pi][pj][py] == 0)
-                {
-                    fruit[pi][pj][py] = fruit[fronti][frontj][fronty] + 1;
-                    q.push({{pi, pj}, py});
+        for(int i = 0; i < 6; i++){
+            int tempx = nx + dx[i];
+            int tempy = ny + dy[i];
+            int tempz = nz + dz[i];
+            
+            if(tempx >= 0 && tempx < H && 
+               tempy >= 0 && tempy < M && 
+               tempz >= 0 && tempz < N){
+                if(arr[tempx][tempy][tempz] == 0){
+                    arr[tempx][tempy][tempz] = arr[nx][ny][nz] + 1;
+                    q.push({{tempx, tempy}, tempz});
                 }
+                    
             }
         }
     }
 }
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     cin >> N >> M >> H;
-
-    for (int i = 0; i < H; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            for (int y = 0; y < N; y++)
-            {
-                cin >> fruit[i][j][y];
+    
+    for(int i = 0; i < H; i++){
+        for(int j = 0; j < M; j++){
+            for(int k = 0; k < N; k++){
+                cin >> arr[i][j][k];
             }
         }
     }
-
-    for (int i = 0; i < H; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            for (int y = 0; y < N; y++)
-            {
-                if (fruit[i][j][y] == 1)
-                {
-                    q.push({{i, j}, y});
-                }
+    
+    for(int i = 0; i < H; i++){
+        for(int j = 0; j < M; j++){
+            for(int k = 0; k < N; k++){
+                if(arr[i][j][k] == 1) 
+                    q.push({{i, j}, k});
             }
         }
     }
+    
     int d = 0;
     bfs();
-    for (int i = 0; i < H; i++)
-    {
-        for (int j = 0; j < M; j++)
-        {
-            for (int y = 0; y < N; y++)
-            {
-                if (fruit[i][j][y] == 0)
-                {
-                    cout << -1;
+    
+    for(int i = 0; i < H; i++){
+        for(int j = 0; j < M; j++){
+            for(int k = 0; k < N; k++){
+                if(arr[i][j][k] == 0){
+                    cout << -1; 
                     return 0;
-                }
-                else
-                {
-                    if (fruit[i][j][y] > d)
-                    {
-                        d = fruit[i][j][y];
+                } else {
+                    if(arr[i][j][k] > d){
+                        d = arr[i][j][k];
                     }
                 }
             }
