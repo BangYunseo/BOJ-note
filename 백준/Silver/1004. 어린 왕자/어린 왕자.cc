90626bbd405ebbcd248496c9e4ea1bd95c1d7d70
Yunseo_33 <104207412+BangYunseo@users.noreply.github.com>
2025-03-09 13:08:29 +0900

    [Silver III] Title: 어린 왕자, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1004. 어린 왕자/어린 왕자.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int T;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> T;
+    
+    int x1, x2, y1, y2, n, cx, cy, r;
+    while(T--){
+        int count = 0;
+        cin >> x1 >> y1 >> x2 >> y2 >> n;
+        
+        
+        while(n--){
+            cin >> cx >> cy >> r;
+            
+            if((cx - x1) * (cx - x1) + (cy - y1) * (cy - y1) < r * r)
+                if ((cx - x2) * (cx - x2) + (cy - y2) * (cy - y2) > r * r)
+                    count++;
+            
+            if((cx - x1) * (cx - x1) + (cy - y1) * (cy - y1) > r * r)
+                if ((cx - x2) * (cx - x2) + (cy - y2) * (cy - y2) < r * r)
+                    count++;
+        }
+        
+        cout << count << '\n';
+    }
+}
